fae62306f77345c36443951605c3fff00827a58a
cucoplus <62451022+cucoplus@users.noreply.github.com>
2020-03-21 01:09:27 +0100

    Add files via upload

---
+++ en.js
@@ -0,0 +1,61 @@
+module.exports = {
+  LANGUAGE_CODE: 'en',
+  ERROR: `Error`,
+  ERROR_404_BODY: `Sorry, the page you requested could not be found.`,
+  ERROR_500_BODY: `Sorry, your request couldn't be processed at this moment.`,
+  LIPSUM: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin maximus sem ante, sit amet semper mauris venenatis non. Fusce posuere, libero ut volutpat ornare, massa ipsum hendrerit ipsum, sed finibus ipsum augue a nisl. Etiam at tempus ante, sit amet tristique leo. Vestibulum lobortis nisl a volutpat rhoncus. Ut facilisis ante a iaculis sollicitudin. Ut consectetur magna at libero vulputate, id pulvinar dui rutrum. Etiam facilisis finibus tincidunt.`,
+  BY_NAME: `XPRL Labs`,
+  XRPL_SIGNPLATFORM: `XRPL Signing Platform (BETA)`,
+  TITLE_SIGNTX: `Sign request`,
+  ALREADY_SIGNED: `Already resolved`,
+  PAYLOAD_RESOLVED: `This request has already been resolved (signed or rejected).`,
+  SCAN_WITH_APP: `Scan this QR with <b class="font-logo">xumm</b>`,
+  PAYLOAD_NOT_FOUND: `Sign request not found.`,
+  PAYLOAD_EXPIRED: `Sign request expired.`,
+  PAYLOAD_BACKEND_ERROR: `Sorry, an error occurred while fetching the data to be signed.`,
+  PROFILE_BACKEND_ERROR: `Sorry, an error occurred while fetching the user profile data.`,
+  PAYLOAD_PAGE_FOR_MOBILE: `To sign a request, make sure you have the <b class="font-logo">xumm</b> app installed.`,
+  APPLICATION_DISABLED: `The application <q><b>__appname__</b></q> is disabled.`,
+  WAIT_CONNECT_PLATFORM: `Connecting to the Signing Platform.`,
+  WAIT_OPEN: `Waiting for you to open the 
+                  <code class="text-primary"><b class="text-primary"><q>__txtype__</q></b></code>
+                  request with the XUMM app.`,
+  WAIT_SCAN_QR: `Waiting for you to scan the 
+                  <code class="text-primary"><b class="text-primary"><q>__txtype__</q></b></code>
+                  request with the XUMM app.`,
+  WAIT_SCAN_QR_DETAILED: `Waiting for you to scan and open the 
+                            <code class="text-primary"><b class="text-primary">__txtype__</b></code>
+                          to 
+                            <code class="text-primary"><b>__txdestination__</b></code>
+                          with the XUMM app.`,
+  WAIT_FOR_RESOLVE: `Waiting for you to resolve (accept / deny) the 
+                      <code class="text-primary"><b class="text-primary"><q>__txtype__</q></b></code>
+                      request in XUMM app.`,
+  SHOW_TRANSACTION: `Show sign request`,
+  RESOLVED: `The sign request has been signed succesfully.`,
+  RESOLVED_NEXT_RETURN: `You will return to <q><b>__appname__</b></q> in a few seconds`,
+  REJECTED: `The sign request has been rejected.`,
+  REJECTED_NEXT_RETURN: `You will return to <q><b>__appname__</b></q> in a few seconds`,
+  GET_APP_IOS_TITLE: `Get 𝘅𝘂𝗺𝗺 for iOS at the Apple App Store`,
+  GET_APP_ANDROID_TITLE: `Get 𝘅𝘂𝗺𝗺 for Android at the Google Play Store`,
+  MODAL_CLOSE: `Close`,
+  GET_APP_IOS_MODAL_TITLE: `Download <b class="font-logo">xumm</b> for iOS`,
+  GET_APP_ANDROID_MODAL_TITLE: `Download <b class="font-logo">xumm</b> for Android`,
+  GET_APP_IOS_MODAL_SCAN: `Scan the QR with your iOS device to visit the <i class="fab fa-apple"></i> App Store.`,
+  GET_APP_ANDROID_MODAL_SCAN: `Scan the QR with your Android device to visit the <i class="fab fa-google-play"></i> Google Play Store.`,
+  GET_APP_IOS_MODAL_VISIT: `Visit the Apple App Store`,
+  GET_APP_ANDROID_MODAL_VISIT: `Visit the Google Play Store`,
+  PAYLOAD_PUSH_NOTIFICATION_SENT: `A push notification has been sent to your mobile device(s).`,
+  DIDNT_RECEIVE_PUSH_TEXT: `Didn't receive the push message?`,
+  SHOW_QR: `Scan a QR code instead`,
+  OPEN_IN_XUMM: 'Open in the <b class="font-logo">xumm</b> App',
+  PLEASE_INSTALL_APP: `Please install <b class="font-logo">xumm</b>`,
+  DASH_OR: '-or-',
+  INSTALL_APP: `Install <b class="font-logo">xumm</b>`,
+  TITLE_TX_SCANNED: `Opened in <b class="font-logo">xumm</b>`,
+  PAYLOAD_ALREADY_OPENED: `Sign request has been opened`,
+  NEED_QR_PAYLOAD_QR: `You can reopen the sign request. The sign request expires <span class="ts-moment">__in_x_time_expire__</span>.`,
+  PUSH_MSG_SIGN_REQUEST: `Sign request`,
+  PUSH_MSG_TXTYPE_TO_DEST: `__type__ to __destination__`,
+  PUSH_MSG_SIGNIN_REQ: `Sign In to __appname__`
+}
